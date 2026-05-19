0e7f7d63a72e66d88ebd6a16b1037923a4236aa7
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-19 16:18:55 +0200

    feat: Composable useLabels y Store. API <<--(mediante useQuery) --Composable ----> Store

---
+++ src/stores/issues.ts
@@ -0,0 +1,25 @@
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+
+export const useIssuesStore = defineStore('issues', () => {
+  const state = ref(''); // all = '', open, closed
+  const labels = ref<string[]>([]);
+
+  return {
+    // State
+    state,
+    labels,
+
+    // Getters
+
+    // Actions
+    toggleLabel(labelName: string) {
+      if (labels.value.includes(labelName)) {
+        labels.value = labels.value.filter((label) => label !== labelName);
+        return;
+      }
+
+      labels.value.push(labelName);
+    },
+  };
+});
